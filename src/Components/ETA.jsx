e04b90b3a1f10b5cd34ebf079ce202e541293b66
alexander-p1 <alexanderpettersson_@hotmail.com>
2025-03-10 22:02:56 +0100

    eta typ färdig

--- src/Components/ETA.jsx
+++ src/Components/ETA.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import "/src/styling/ETA.scss"
+import group from '../assets/Group 6.svg'
+import boxtop from '../assets/boxtop 1.png'
 
 const ETA = () => {
 
   return (
     <div className='containerEta'>
+      <header><img src={group} alt="" className='headerEta'/></header>
+      <section className='etaSection'>
+        <img src={boxtop} alt="" className='boxtopEta' />
+      </section>
+
+      <section className='etaSection'>
+      <h1>Dina wontons tillagas!</h1>
+      </section>
+      
+      <footer className='footerBtn'>
+        <button>GÖR EN NY BESTÄLLNING</button>
+        <button>SE KVITTO</button>
+      </footer>
     </div>
   )
 }
